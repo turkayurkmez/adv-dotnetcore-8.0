8efcc7a1b3c0f09c691b43bd124a6f24f9bc6d02
türkay ürkmez <turkay.urkmez@outlook.com>
2025-03-27 15:08:35 +0300

    using SeriLog in asp.net core

---
+++ usingLogging/usingLogging/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace usingLogging.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("*** Get request talebi geldi... *****");
+            try
+            {
+                _logger.LogDebug("Hava durumunu çekme işlemi başladı....");
+                var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToArray();
+                _logger.LogInformation("*** Başarıyla veri çekildi *****");
+                return weatherForecasts;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Hata oluştu", ex.Message);
+                throw;
+                
+            }
+
+
+        }
+    }
+}
---
+++ usingLogging/usingLogging/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Enrichers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Logging.AddEventLog();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName() // Ensure you have the Serilog.Enrichers.Environment package installed
+    .Enrich.WithEnvironmentName()
+    .WriteTo.Console(outputTemplate:"[{Timestampt:HH:mm:ss} {Level: u3}] {SourceContext} {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+//builder.Logging.AddSerilog(Log.Logger);
+builder.Host.UseSerilog();
+
+//builder.Logging.AddFilter("System", LogLevel.Warning);
+//builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
+//builder.Logging.AddFilter("WeatherForecastController", LogLevel.Debug);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var info = app.Configuration.GetValue<string>("Info");
+app.Logger.LogInformation($"Belgedeki değer: {info}");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
