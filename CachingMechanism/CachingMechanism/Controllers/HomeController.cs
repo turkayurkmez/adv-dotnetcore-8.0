79e970cf39864068a427d50b6178f107d2180bb5
türkay ürkmez <turkay.urkmez@outlook.com>
2025-03-27 12:11:14 +0300

    Caching Mechanism on asp.net

---
+++ CachingMechanism/CachingMechanism/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics;
+using CachingMechanism.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CachingMechanism.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _cache;
+
+        public HomeController(ILogger<HomeController> logger, IMemoryCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+          var options = new MemoryCacheEntryOptions();
+            //TTL (Time to Live) - Cache'te ne kadar süre kalacağını belirler:"
+            //sadece birini kullanabiliriz:
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(60);
+            //options.SlidingExpiration = TimeSpan.FromSeconds(30);
+            //Cache'ten veri silindiğinde çalışacak metot
+            //öncelik belirleme:
+            //options.SetPriority
+
+
+
+
+
+            options.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration
+            {
+                EvictionCallback = (key, value, reason, state) =>
+                {
+                    
+                    _logger.LogInformation($"Cache entry was removed. Reason: {reason}");
+                }
+            });
+            //Eğer cache'te veri yoksa cache'e ekleyip döndür (Lazy initialization)
+            if (!_cache.TryGetValue("Employees", out List<Employee> employees))
+            {
+                employees = new List<Employee>
+                {
+                    new Employee { Id = 1, Name = "Ahmet", Department = "IT" },
+                    new Employee { Id = 2, Name = "Mehmet", Department = "HR" },
+                    new Employee { Id = 3, Name = "Ayşe", Department = "IT" },
+                    new Employee { Id = 4, Name = "Fatma", Department = "HR" }
+                };
+
+
+                _cache.Set("Employees", employees, TimeSpan.FromSeconds(60));
+            }
+
+            ViewBag.Employees = employees;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var employees = _cache.Get<List<Employee>>("Employees");
+            ViewBag.Employees = employees;
+            return View();
+        }
+
+        
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+        [HttpGet("api/employees/{id}")]
+        public IActionResult Get(int id)
+        {
+            var employee = new Employee
+            {
+                Id = id,
+                Name = "Türkay",
+                Department = "Finans"
+            };
+
+            return Ok(new { data = employee, DateInfo = DateTime.Now });
+        }
+
+        public IActionResult CacheHelper(int? id)
+        {
+            Employee employee = null;
+            if (id.HasValue)
+            {
+                employee = new Employee
+                {
+                    Id = id.Value,
+                    Name = "Ayşe",
+                    Department = "Yazılım"
+                };
+            }
+            else
+            {
+                employee = new Employee
+                {
+                    Id = 5,
+                    Name = "Türkay",
+                    Department = "Finans"
+                };
+            }
+
+
+            return View(employee);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- OptionsPattern/OptionsPattern/Program.cs
+++ OptionsPattern/OptionsPattern/Program.cs
@@ -1,4 +1,4 @@
-using OptionsPattern.Models;
+﻿using OptionsPattern.Models;
 using OptionsPattern.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +11,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+/*
+ * appsettings/json dosyayindaki veriyi EmailSettings nesnesine bind etmek ve bu nesneyi
+ * herhangi bir nesneye enjekte etmek için Configure metodu kullanılır.
+ * 
+ * Eğer sadece bind etmek isteseydim:
+ * builder.Configuration.GetSection("EmailSettings").Bind(new EmailSettings());
+   builder.Configuration.Get<EmailSettings>();
+ */
+
+
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
 builder.Services.AddSingleton<MailService>();
 builder.Services.AddScoped<ConfigAwareService>();
 
